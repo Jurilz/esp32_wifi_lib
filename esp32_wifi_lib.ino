ed4a417cb99d88ef4a705aeb930eb35b82a9e9bf
Juri <juri.lozowoj@freenet.de>
2022-02-05 18:42:45 +0100

    initial commit

---
+++ esp32_wifi_lib.ino
@@ -0,0 +1,37 @@
+#include "WiFi.h"
+
+char ssid[] = "yourNetwork";     //  your network SSID (name)
+char pass[] = "secretPassword";  // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+  Serial.begin(115200);
+
+  checkForWifiShield();
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WEP network, SSID: ");
+  }
+ 
+}
+
+void tryToConnect(){
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+boolean checkForWifiShield(){
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("Your ESP32 has no shield, so I'm doing nothing");
+    stopSketch();
+  }
+}
+
+void stopSketch() {
+  noInterrupts();
+  while(true) {}
+}
